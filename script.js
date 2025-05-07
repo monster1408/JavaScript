d6e59993b3fc8ee497518a69af4bc1ed65e38fb0
monster1408 <yukiya.toyohara@hotmail.com>
2025-05-07 20:51:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 function run(){
   let code = document.getElementById("codeInput").value;
   code = code + 'document.getElementById("run").innerHTML = output;'
+  code = code.replace("<br>", "");
   document.getElementById("CodeSspace").innerText = code;
 }
